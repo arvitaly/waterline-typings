b2940435975197c5725d2d462ebefefd74b4971f
Alexandro <arvitaly@list.ru>
2016-11-14 05:14:21 +0700

    init

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,33 @@
 declare namespace Waterline {
-    interface Waterline {
+    interface Adapter {
 
     }
+    type Connection = {
+        adapter: string;
+    }
+    interface Config {
+        adapters: { [index: string]: Adapter };
+        connections: { [index: string]: Connection }
+    }
+    type Ontology<T> = {
+        collections: T;
+    }
+    interface Waterline<T> {
+        loadCollection(collection: Collection);
+        initialize: (config: Config, cb: (err: Error, ontology: Ontology<T>) => any) => any;
+        collections: T;
+    }
+    interface WaterlineStatic {
+        Collection: {
+            extend: (params: Collection) => Collection;
+        }
+        new <T>(): Waterline<T>;
+    }
     export type BaseAttribute = {
-        autoIncrement: boolean;
-        primaryKey: boolean;
-        unique: boolean;
+        type?: string;
+        autoIncrement?: boolean;
+        primaryKey?: boolean;
+        unique?: boolean;
     }
     export type StringAttribute = BaseAttribute & {
         type: 'string';
@@ -24,13 +46,88 @@ declare namespace Waterline {
     }
     export type Attribute = string | StringAttribute | IntegerAttribute | ModelAttribute | DatetimeAttribute;
     export type Attributes = { [index: string]: Attribute };
-    export type Model = {
+
+    export interface BaseModel<T> {
+        create: (model: T) => Promise<T>;
+        update: (conditions: T, model: T) => Promise<Array<T>>;
+        find: (model: T) => Promise<Array<T>>;
+    }
+    export interface Model<T> extends BaseModel<T> {
+        connections
+        waterline: Waterline<any>;
+        adapter
+        defaults
+        _cast
+        _schema
+        _validator
+        _callbacks
+        _instanceMethods
+        hasSchema
+        migrate
+        _model
+        _transformer
+        adapterDictionary
+        pkFormat
+        syncable
+        registerConnection
+        teardown
+        define
+
+        findOneById
+        findOneByIdIn
+        findOneByIdLike
+        findById
+        findByIdIn
+        findByIdLike
+        countById
+        countByIdIn
+        countByIdLike
+        idStartsWith
+        idContains
+        idEndsWith
+        findOneByCreatedAt
+        findOneByCreatedAtIn
+        findOneByCreatedAtLike
+        findByCreatedAt
+        findByCreatedAtIn
+        findByCreatedAtLike
+
+        countByCreatedAt
+        countByCreatedAtIn
+        countByCreatedAtLike
+        createdAtStartsWith
+        createdAtContains
+        createdAtEndsWith
+        findOneByUpdatedAt
+        findOneByUpdatedAtIn
+        findOneByUpdatedAtLike
+        findByUpdatedAt
+        findByUpdatedAtIn
+        findByUpdatedAtLike
+        countByUpdatedAt
+        countByUpdatedAtIn
+        countByUpdatedAtLike
+        updatedAtStartsWith
+        updatedAtContains
+        updatedAtEndsWith
+        definition
+        meta
+
         _attributes: Attributes;
+    }
+    type CollectionAttribute = Attribute;
+    export type Collection = {
+        attributes?: { [index: string]: CollectionAttribute };
+        connection?: string;
+        identity?: string;
+        tableName?: string;
+        migrate?: "alter" | "drop" | "safe";
+        autoPK?: boolean;
+        autoCreatedAt?: Date;
+        autoUpdatedAt?: Date;
     }
 }
 declare module 'waterline' {
-    var waterline: Waterline.Waterline;
+    var waterline: Waterline.WaterlineStatic;
     export = waterline;
 }
-export = Waterline;
-export as namespace Waterline;
